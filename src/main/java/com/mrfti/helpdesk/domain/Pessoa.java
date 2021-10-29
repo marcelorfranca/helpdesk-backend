6515e740d7f2ca91379c66828c91273679bc4aed
marcelorfranca <marcelorfranca@hotmail.com>
2021-10-29 11:41:19 -0300

    classes de dominio iniciados

---
+++ src/main/java/com/mrfti/helpdesk/domain/Pessoa.java
@@ -0,0 +1,112 @@
+package com.mrfti.helpdesk.domain;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.mrfti.helpdesk.domain.enums.Perfil;
+
+public abstract class Pessoa {
+
+	protected Integer id; // protected somente as classes Cliente e Tecnico terao acesso
+	protected String nome;
+	protected String cpf;
+	protected String email;
+	protected String senha;
+	protected Set<Integer> perfis  = new HashSet<>(); // Set impede dois valores iguais 
+	// hashset evita erro de ponteiro nulo
+	protected LocalDate dataCriacao = LocalDate.now();
+	
+	public Pessoa() {
+		super();
+		addPerfil(Perfil.CLIENTE);
+	}
+
+	public Pessoa(Integer id, String nome, String cpf, String email, String senha) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+		this.senha = senha;
+		addPerfil(Perfil.CLIENTE);
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Set<Perfil> getPerfis() { // tipo Perfil altado para Integer. Chamado o metodo la de Perfil
+		return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+	}
+
+	public void addPerfil(Perfil perfil) { // alterado para add e lista
+		this.perfis.add(perfil.getCodigo());
+	}
+
+	public LocalDate getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(LocalDate dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpf, id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		return Objects.equals(cpf, other.cpf) && Objects.equals(id, other.id);
+	}
+
+	
+	
+}
