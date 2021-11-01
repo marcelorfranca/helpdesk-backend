29b3d003a633544ce3952f8ee4fdf6d5e732841c
marcelorfranca <marcelorfranca@hotmail.com>
2021-11-01 19:05:50 -0300

    Criando Tecnico findById

---
+++ src/main/java/com/mrfti/helpdesk/resources/TecnicoResource.java
@@ -0,0 +1,25 @@
+package com.mrfti.helpdesk.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mrfti.helpdesk.domain.Tecnico;
+import com.mrfti.helpdesk.services.TecnicoService;
+
+@RestController
+@RequestMapping(value="/tecnicos")
+public class TecnicoResource {
+	
+	@Autowired
+	private TecnicoService service;
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<Tecnico> findById(@PathVariable Integer id) { // representa toda a resposta http. tipo da resposta é <Tecnico>
+		Tecnico obj  = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
