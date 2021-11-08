e9cd8a8e7b38c5769f60bad6637c6b1feee67fa2
marcelorfranca <marcelorfranca@hotmail.com>
2021-11-08 12:04:05 -0300

    Update para tecnico implementado

--- src/main/java/com/mrfti/helpdesk/services/TecnicoService.java
+++ src/main/java/com/mrfti/helpdesk/services/TecnicoService.java
@@ -3,6 +3,8 @@ package com.mrfti.helpdesk.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -39,6 +41,14 @@ public class TecnicoService {
 		return repository.save(newObj);
 	}
 
+	public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
+		objDTO.setId(id);
+		Tecnico oldObj = findById(id);
+		validaPorCpfEEmail(objDTO);
+		oldObj = new Tecnico(objDTO);
+		return repository.save(oldObj);
+	}
+	
 	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
 		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
@@ -53,5 +63,7 @@ public class TecnicoService {
 		
 		
 	}
+
+	
 	
 }
