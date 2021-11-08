b7545cc8517406930f31d0f4b60dcfb23824e63c
marcelorfranca <marcelorfranca@hotmail.com>
2021-11-08 18:43:35 -0300

    Crud CLIENTE implementado

---
+++ src/main/java/com/mrfti/helpdesk/services/ClienteService.java
@@ -0,0 +1,82 @@
+package com.mrfti.helpdesk.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mrfti.helpdesk.domain.Cliente;
+import com.mrfti.helpdesk.domain.Pessoa;
+import com.mrfti.helpdesk.domain.dtos.ClienteDTO;
+import com.mrfti.helpdesk.repositories.ClienteRepository;
+import com.mrfti.helpdesk.repositories.PessoaRepository;
+import com.mrfti.helpdesk.services.exceptions.DataIntegrityViolationException;
+import com.mrfti.helpdesk.services.exceptions.ObjectnotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repository;
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	public Cliente findById(Integer id) {
+		Optional<Cliente> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objecto não encontrado! Id: " + id));
+	}
+
+	public List<Cliente> findAll() {
+		return repository.findAll();
+	}
+
+	public Cliente create(ClienteDTO objDTO) { // converter as infos para novo Cliente
+		objDTO.setId(null); // assegurando que o ID virá nulo, senao ele pensará que é um update
+		validaPorCpfEEmail(objDTO);
+		Cliente newObj = new Cliente(objDTO);
+		return repository.save(newObj);
+	}
+
+	public Cliente update(Integer id, @Valid ClienteDTO objDTO) {
+		objDTO.setId(id);
+		Cliente oldObj = findById(id);
+		validaPorCpfEEmail(objDTO);
+		oldObj = new Cliente(objDTO);
+		return repository.save(oldObj);
+	}
+	
+	
+	public void delete(Integer id) {
+		Cliente obj = findById(id); // verifica se o id existe usando este método já criado
+		if(obj.getChamados().size() > 0 ) { // se houver chamado atrelado não permitirá exclusão
+			throw new DataIntegrityViolationException("Cliente possui ordens de serviço e não pode ser deletado!");
+		} //senão
+		
+		repository.deleteById(id);
+		
+	}
+	
+	private void validaPorCpfEEmail(ClienteDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
+		}
+		
+		// verifica o email somente se o cpf passar
+		obj = pessoaRepository.findByEmail(objDTO.getEmail());
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("EMAIL já cadastrado no sistema!");
+		}
+		
+		
+	}
+
+	
+
+	
+	
+}
