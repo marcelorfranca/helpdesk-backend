094ddaebfe79e1ecc92632cf77b1382c8a8a6325
marcelorfranca <marcelorfranca@hotmail.com>
2021-11-11 17:16:45 -0300

    findAll para Chamado

--- src/main/java/com/mrfti/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/mrfti/helpdesk/resources/ChamadoResource.java
@@ -1,5 +1,8 @@
 package com.mrfti.helpdesk.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,4 +27,13 @@ public class ChamadoResource {
 		return ResponseEntity.ok().body(new ChamadoDTO(obj));
 	}
 
+	@GetMapping
+	public ResponseEntity<List<ChamadoDTO>> findAll() {
+		List<Chamado> lista = service.findAll();
+		List<ChamadoDTO> listaDTO = lista.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList()); // convertendo para DTO
+		return ResponseEntity.ok().body(listaDTO);
+	}
+	
+	
+	
 }
--- src/main/java/com/mrfti/helpdesk/services/ChamadoService.java
+++ src/main/java/com/mrfti/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.mrfti.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,10 @@ public class ChamadoService {
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! ID: " + id));
+	}
+
+	public List<Chamado> findAll() {
+		return repository.findAll();
 	}
 	
 	
